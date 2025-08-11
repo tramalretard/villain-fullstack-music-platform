f0cce5f6935c4558870b9045de103c93203ea05c
bloodsense <iknowozey@gmail.com>
2025-08-11 11:40:13 +0700

    server(db): database migration created

---
+++ server-side/prisma/migrations/20250811043451_init/migration.sql
@@ -0,0 +1,158 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ARTIST', 'USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "avatar" TEXT NOT NULL DEFAULT '/uploads/no-user-image.png',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "artists" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL DEFAULT '/uploads/no-artist-image.png',
+    "listener" INTEGER NOT NULL DEFAULT 0,
+    "role" "UserRole" NOT NULL DEFAULT 'ARTIST',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "artists_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "playlists" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "imageSrc" TEXT NOT NULL DEFAULT '/uploads/no-playlist-image.png',
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "playlists_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tracks" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "audioSrc" TEXT NOT NULL,
+    "imageSrc" TEXT NOT NULL DEFAULT '/uploads/no-track-image.png',
+    "listener" INTEGER NOT NULL DEFAULT 0,
+    "isExplicit" BOOLEAN NOT NULL DEFAULT false,
+    "artistId" TEXT NOT NULL,
+    "album_id" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "tracks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "albums" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "imageSrc" TEXT NOT NULL DEFAULT '/uploads/no-album-image.png',
+    "isExplicit" BOOLEAN NOT NULL DEFAULT false,
+    "listener" INTEGER NOT NULL DEFAULT 0,
+    "artist_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "albums_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_PlaylistToTrack" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_PlaylistToTrack_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_UserFavoriteTracks" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_UserFavoriteTracks_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_UserFavoriteAlbums" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_UserFavoriteAlbums_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "artists_email_key" ON "artists"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "artists_name_key" ON "artists"("name");
+
+-- CreateIndex
+CREATE INDEX "playlists_user_id_idx" ON "playlists"("user_id");
+
+-- CreateIndex
+CREATE INDEX "tracks_artistId_idx" ON "tracks"("artistId");
+
+-- CreateIndex
+CREATE INDEX "tracks_album_id_idx" ON "tracks"("album_id");
+
+-- CreateIndex
+CREATE INDEX "albums_artist_id_idx" ON "albums"("artist_id");
+
+-- CreateIndex
+CREATE INDEX "_PlaylistToTrack_B_index" ON "_PlaylistToTrack"("B");
+
+-- CreateIndex
+CREATE INDEX "_UserFavoriteTracks_B_index" ON "_UserFavoriteTracks"("B");
+
+-- CreateIndex
+CREATE INDEX "_UserFavoriteAlbums_B_index" ON "_UserFavoriteAlbums"("B");
+
+-- AddForeignKey
+ALTER TABLE "playlists" ADD CONSTRAINT "playlists_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tracks" ADD CONSTRAINT "tracks_artistId_fkey" FOREIGN KEY ("artistId") REFERENCES "artists"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tracks" ADD CONSTRAINT "tracks_album_id_fkey" FOREIGN KEY ("album_id") REFERENCES "albums"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "albums" ADD CONSTRAINT "albums_artist_id_fkey" FOREIGN KEY ("artist_id") REFERENCES "artists"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PlaylistToTrack" ADD CONSTRAINT "_PlaylistToTrack_A_fkey" FOREIGN KEY ("A") REFERENCES "playlists"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PlaylistToTrack" ADD CONSTRAINT "_PlaylistToTrack_B_fkey" FOREIGN KEY ("B") REFERENCES "tracks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserFavoriteTracks" ADD CONSTRAINT "_UserFavoriteTracks_A_fkey" FOREIGN KEY ("A") REFERENCES "tracks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserFavoriteTracks" ADD CONSTRAINT "_UserFavoriteTracks_B_fkey" FOREIGN KEY ("B") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserFavoriteAlbums" ADD CONSTRAINT "_UserFavoriteAlbums_A_fkey" FOREIGN KEY ("A") REFERENCES "albums"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserFavoriteAlbums" ADD CONSTRAINT "_UserFavoriteAlbums_B_fkey" FOREIGN KEY ("B") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
