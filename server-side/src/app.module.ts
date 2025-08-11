d2e97650526bc10b3323ed03c6c260439e66afae
bloodsense <iknowozey@gmail.com>
2025-08-11 11:08:11 +0700

    server(feat): prisma is initialized, cors, cookie-parser and configModule are connected

--- server-side/src/app.module.ts
+++ server-side/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
 })
 export class AppModule {}
--- server-side/src/main.ts
+++ server-side/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
+  app.use(cookieParser());
+
   await app.listen(5000);
 }
 bootstrap();
