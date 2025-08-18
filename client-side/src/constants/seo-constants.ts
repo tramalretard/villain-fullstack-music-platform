f2216e59c3f777f7cda5140ae9e6b9a1d8d20079
bloodsense <iknowozey@gmail.com>
2025-08-19 01:21:24 +0700

    client(update): globals.css and favicon changed, SEO constants added

---
+++ client-side/src/constants/seo-constants.ts
@@ -0,0 +1,6 @@
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
+
+export const SITE_NAME = 'VILLAIN'
+
+export const SITE_DESCRIPTION =
+	'Онлайн-сервис, предоставляющий доступ к широкому каталогу музыкальных треков, альбомов и плейлистов для прослушивания'
