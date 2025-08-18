45264367149ad502dd109df50b780dba9dededab
bloodsense <iknowozey@gmail.com>
2025-08-19 01:33:39 +0700

    client(update): config file configured, environment variable file example added

--- client-side/next.config.ts
+++ client-side/next.config.ts
@@ -1,7 +1,34 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	env: {
+		APP_ENV: process.env.APP_ENV,
+		APP_URL: process.env.APP_URL,
+		APP_DOMAIN: process.env.APP_DOMAIN,
+		SERVER_URL: process.env.SERVER_URL,
+	},
 
-export default nextConfig;
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'avatars.githubusercontent.com',
+			},
+		],
+	},
+
+	async rewrites() {
+		return [
+			{
+				source: '/uploads/:path*',
+				destination: `${process.env.SERVER_URL}/uploads/:path*`,
+			},
+		]
+	},
+}
+
+export default nextConfig
